e66341af465fe66304cb0388e3de000adb43ab3d
NV9C <55490325+NV9C@users.noreply.github.com>
2020-06-02 13:59:27 +0530

    Add files via upload
    Welcome to the world of Java! In this challenge, we practice printing to stdout.
    
    The code stubs in your editor declare a Solution class and a main method. Complete the main method by copying the two lines of code below and pasting them inside the body of your main method.
    
    System.out.println("Hello, World.");
    System.out.println("Hello, Java.");
    Input Format
    
    There is no input for this challenge.
    
    Output Format
    
    You must print two lines of output:
    
    Print Hello, World. on the first line.
    Print Hello, Java. on the second line.
    Sample Output
    
    Hello, World.
    Hello, Java

---
+++ Demo.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Demo
+{
+	public static void main (String[] args)
+	{
+	
+System.out.println("Hello, World");
+System.out.println("Hello, Java");
+
+
+	}
+}
