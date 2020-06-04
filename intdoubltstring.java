1cf78786791f088296e1f0df9d7fe9c476c1344a
NV9C <55490325+NV9C@users.noreply.github.com>
2020-06-04 21:20:03 +0530

    Add files via upload
    In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.
    
    Note: We recommend completing Java Stdin and Stdout I before attempting this challenge.
    
    Input Format
    
    There are three lines of input:
    
    The first line contains an integer.
    The second line contains a double.
    The third line contains a String.
    Output Format
    
    There are three lines of output:
    
    On the first line, print String: followed by the unaltered String read from stdin.
    On the second line, print Double: followed by the unaltered double read from stdin.
    On the third line, print Int: followed by the unaltered integer read from stdin.
    To make the problem easier, a portion of the code is already provided in the editor.
    
    Note: If you use the nextLine() method immediately following the nextInt() method, recall that nextInt() reads integer tokens; because of this, the last newline character for that line of integer input is still queued in the input buffer and the next nextLine() will be reading the remainder of the integer line (which is empty).
    
    Sample Input
    
    42
    3.1415
    Welcome to HackerRank's Java tutorials!
    Sample Output
    
    String: Welcome to HackerRank's Java tutorials!
    Double: 3.1415
    Int: 42

---
+++ intdoubltstring.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Hello
+{
+	public static void main (String[] args)
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int a=sc.nextInt();
+	  double b=sc.nextDouble();
+	  sc.nextLine();
+	  String s=sc.nextLine();
+	  System.out.println("String:"+s);
+	  System.out.println("Double:"+b);
+	  System.out.println("Int:"+a);
+	  
+	  
+	
+	}
+}
