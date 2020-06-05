9bd06dd10b865705ab3548319bbcccb27d613fb4
NV9C <55490325+NV9C@users.noreply.github.com>
2020-06-05 13:18:44 +0530

    Add files via upload
    Objective
    In this challenge, we're going to use loops to help us do some simple math.
    
    Task
    Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
    
    Input Format
    
    A single integer, .
    
    Output Format
    
    Print  lines of output; each line  (where ) contains the  of  in the form:
    N x i = result.
    
    Sample Input
    
    2
    Sample Output
    
    2 x 1 = 2
    2 x 2 = 4
    2 x 3 = 6
    2 x 4 = 8
    2 x 5 = 10
    2 x 6 = 12
    2 x 7 = 14
    2 x 8 = 16
    2 x 9 = 18
    2 x 10 = 20

---
+++ table.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Table
+{
+	public static void main (String[] args)
+	{
+		int i;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(i=1;i<=10;i++)
+		{
+			
+		  System.out.println(n+ "*" +i+"="+(n*i));
+		  }
+	}
+}
