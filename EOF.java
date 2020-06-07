9473e3232f70d9369dc90ce501230e10a213d6b6
NV9C <55490325+NV9C@users.noreply.github.com>
2020-06-07 13:51:04 +0530

    Add files via upload
    The challenge here is to read  lines of input until you reach EOF, then number and print all  lines of content.
    
    Hint: Java's Scanner.hasNext() method is helpful for this problem.
    
    Input Format
    
    Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
    
    Output Format
    
    For each line, print the line number, followed by a single space, and then the line content received as input.
    
    Sample Input
    
    Hello world
    I am a file
    Read me until end-of-file.
    Sample Output
    
    1 Hello world
    2 I am a file
    3 Read me until end-of-file.

---
+++ EOF.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Demo
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner scan = new Scanner(System.in); 
+	   int i = 0; 
+	   while(scan.hasNext())
+	   { 
+	   	i++;
+	   	 System.out.println(i + " " + scan.nextLine()); 
+	   	 }
+	     }
+	}
+
