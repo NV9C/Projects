1a09cf6b4a17dab3fcd7a7bd27feedf76ea96482
NV9C <55490325+NV9C@users.noreply.github.com>
2020-06-09 13:51:12 +0530

    Add files via upload
    You are given an integer , you have to convert it into a string.
    
    Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
    
     can range between  to  inclusive.
    
    Sample Input 0
    
    100
    Sample Output 0
    
    Good job

---
+++ intToString.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Demo
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner (System.in);
+		int n=sc.nextInt();
+		String s=Integer.toString(n);
+		String r=String.valueOf(n);
+		System.out.println(n+1);
+		System.out.println(s+1);
+		System.out.println(r+1);
+	}
+}
