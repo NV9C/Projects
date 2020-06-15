7cfa1106617121f967803c912c5da952ebc7673c
NV9C <55490325+NV9C@users.noreply.github.com>
2020-06-16 01:57:11 +0530

    Add files via upload
    Static used

---
+++ static.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class D
+{
+	
+	public static void main (String[] args)
+	{
+		int n=2,i;
+		for(i=1;i<=10;i++)
+		{
+			System.out.println(n+"*"+i+"="+n*i);
+			}
+	}
+}
