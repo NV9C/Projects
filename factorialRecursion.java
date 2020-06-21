5e2ebf008058f701494b57c81fbd4c0e568d7466
NV9C <55490325+NV9C@users.noreply.github.com>
2020-06-22 01:12:07 +0530

    Add files via upload
    Factorial using Recursion

---
+++ factorialRecursion.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class D
+{
+	public static void main (String[] args)
+	{
+		 int n=5,f=1,i;
+		 for(i=1;i<6;i++)
+		 {
+		 	f=f*i;
+	
+		    System.out.println(f);
+		    }
+	}
+}
