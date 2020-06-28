370f8e865b726a07a6127990072c93af72b751dc
NV9C <55490325+NV9C@users.noreply.github.com>
2020-06-28 16:07:20 +0530

    Add files via upload
    Insert head

---
+++ insertHead.java
@@ -0,0 +1,15 @@
+/*
+  Insert Node at the beginning of a linked list 
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+
+Node Insert(Node head, int x) {
+    Node n = new Node();
+    n.data = x;
+    n.next = head;
+    return n;
+}
