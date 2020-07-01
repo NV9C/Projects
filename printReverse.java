f6b3a4e7659dec77c33709ab9f188d4fcc76aa66
NV9C <55490325+NV9C@users.noreply.github.com>
2020-07-02 01:20:13 +0530

    Add files via upload
    Print reverse

---
+++ printReverse.java
@@ -0,0 +1,15 @@
+/* For your reference:
+
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+void reversePrint(SinglyLinkedListNode n) {
+    if (n != null) {
+        reversePrint(n.next);
+        System.out.println(n.data);
+    }
+}
