a29c86cd5d03e83937a849b50c5e7d72fe5c4f1e
NV9C <55490325+NV9C@users.noreply.github.com>
2020-07-07 16:23:17 +0530

    Add files via upload
    Inorder

---
+++ inorder.java
@@ -0,0 +1,16 @@
+/* 
+Node is defined as  
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+*/
+
+void inOrder(Node n) {
+    if (n != null) {
+        inOrder(n.left);
+        System.out.print(n.data + " ");
+        inOrder(n.right);
+    }
+}
