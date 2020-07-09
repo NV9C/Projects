16278d2528a2505997be978b7578e7bbe929ec0a
NV9C <55490325+NV9C@users.noreply.github.com>
2020-07-10 01:34:55 +0530

    Add files via upload
    Postorder

---
+++ postorder.java
@@ -0,0 +1,16 @@
+/* 
+Node is defined as  
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+*/
+
+void postOrder(Node n) {
+    if (n != null) {
+        postOrder(n.left);
+        postOrder(n.right);
+        System.out.print(n.data + " ");
+    }
+}
