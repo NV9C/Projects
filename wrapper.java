72f03cb082cc12df302f0ecb3a9696fb47491dd4
NV9C <55490325+NV9C@users.noreply.github.com>
2020-07-28 13:05:38 +0530

    Add files via upload
    Wrapping

---
+++ wrapper.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class WrapperExample1{  
+
+public static void main(String args[]){  
+int a=20;  
+Integer i=Integer.valueOf(a);
+
+Integer j=a;
+System.out.println(a+" "+i+" "+j);  
+
+}}  
+
