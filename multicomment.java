c8ba2942df74b8e7df3063723e26c1848bd83adf
NV9C <55490325+NV9C@users.noreply.github.com>
2020-08-04 13:18:51 +0530

    Add files via upload
    Multi line comment

---
+++ multicomment.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class CommentExample2 {  
+
+public static void main(String[] args) {  
+
+/* Let's declare and 
+
+ print variable in java. */  
+
+    int i=10;  
+
+    System.out.println(i);  
+
+}  
+
+}  
+
