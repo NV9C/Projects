100df9f359db96d42a41ea5d84f7fcd9f031d22d
NV9C <55490325+NV9C@users.noreply.github.com>
2020-08-05 11:26:52 +0530

    Add files via upload
    Documentation comment

---
+++ documentation.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Calculator {  
+
+/** The add() method returns addition of given numbers.*/  
+
+public static int add(int a, int b){return a+b;}  
+
+/** The sub() method returns subtraction of given numbers.*/  
+
+public static int sub(int a, int b){return a-b;}  
+
+}  
+
