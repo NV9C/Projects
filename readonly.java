05538203489ece2900f87881a48d42e636775dcd
NV9C <55490325+NV9C@users.noreply.github.com>
2020-08-06 00:10:35 +0530

    Add files via upload
    Encapsulation

---
+++ readonly.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Student{  
+
+//private data member  
+
+private String college="AKG";  
+
+//getter method for college  
+
+public String getCollege(){  
+
+return college;  
+
+}  
+
+}  
+
