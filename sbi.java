e9d99fc702fd5cae341962d34df29804e8a9d2ee
NV9C <55490325+NV9C@users.noreply.github.com>
2020-08-07 12:30:30 +0530

    Add files via upload
    Static

---
+++ sbi.java
@@ -0,0 +1,25 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*; 
+public class TestStaticVariable1{  
+
+ public static void main(String args[]){  
+
+ Student s1 = new Student(111,"Karan");  
+
+ Student s2 = new Student(222,"Aryan");  
+
+ //we can change the college of all objects by the single line of code  
+
+ //Student.college="BBDIT";  
+
+ s1.display();  
+
+ s2.display();  
+
+ }  
+
+}
+
